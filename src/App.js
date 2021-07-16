16363ce8cfc18c719c0b80197e9edc8befc92905
Yelison <ortizyelison@gmail.com>
2021-07-16 17:19:56 -0400

    Add automatic invalidation from aws

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
+    console.log('Funciona!');
     return <div>
         <MarketingApp />
     </div>
-}
+};
